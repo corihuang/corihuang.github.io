a14d6a547c082fc10a90d94a51d66788f4cf467a
corihuang <cori.huang@gmail.com>
2020-04-23 02:19:47 -0400

    fixed booklist generating script

--- _plugins/booklist.rb
+++ _plugins/booklist.rb
@@ -1,37 +1,32 @@
 # Generate book list data
 require 'net/http'
 
-module GetBooks
-  class Generator
-    def generate(site)
+dataLocation = "./_data/books.json"
 
-        # Book data file location
-        dataLocation = "./_data/books.json"
+# Raw list of books by ISBN
+isbnList = [
+  "9780062678102",
+  "0988262592",
+  "9780374261597",
+  "9780316229296",
+  "9780316229289"
+]
 
-        # Raw list of books by ISBN
-        isbnList = [
-        "9780062678102",
-        "0988262592",
-        "9780374261597",
-        "9780316229296"
-        ]
+book_url = "https://www.googleapis.com/books/v1/volumes?q="
 
-        book_url = "https://www.googleapis.com/books/v1/volumes?q="
-
-        isbnList.each do |isbn|
-        book_url += "isbn:" + isbn + "+OR+"
-        end
-
-        book_url += "+&fields=items(volumeInfo/description,volumeInfo/title,volumeInfo/authors,volumeInfo/imageLinks/thumbnail,volumeInfo/industryIdentifiers/identifier)+&maxResults=40"
-
-        puts book_url
-
-        uri = URI(book_url)
-        response = Net::HTTP.get(uri)
-
-        file = File.open(dataLocation, "w")
-        file.puts response
-        file.close
-    end
-  end
+isbnList.each do |isbn|
+  book_url += "isbn:" + isbn + "+OR+"
 end
+
+book_url = book_url.chomp("+OR+")
+
+book_url += "+&fields=items(volumeInfo/description,volumeInfo/title,volumeInfo/authors,volumeInfo/imageLinks/thumbnail,volumeInfo/industryIdentifiers/identifier)+&maxResults=40"
+
+puts book_url
+
+uri = URI(book_url)
+response = Net::HTTP.get(uri)
+
+file = File.new(dataLocation, "w")
+file.write(response)
+file.close
